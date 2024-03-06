5491af7f381fdd7cf5a353b49e1eb483fa8f97f0
Hian <101941620+W3SK3RRX@users.noreply.github.com>
2024-03-06 08:46:20 -0300

    Update settings.py

--- setorT/settings.py
+++ setorT/settings.py
@@ -135,7 +135,7 @@ LOGIN_URL = '/auth/login/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/admin/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
