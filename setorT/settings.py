68a75abdf5bfb1cfedacb865e17a3aaee11ae53a
W3SK3RRX <hianclaudio16@gmail.com>
2024-03-07 14:50:17 -0300

    setorT-v2.3.2

--- setorT/settings.py
+++ setorT/settings.py
@@ -134,7 +134,7 @@ LOGIN_URL = '/auth/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
